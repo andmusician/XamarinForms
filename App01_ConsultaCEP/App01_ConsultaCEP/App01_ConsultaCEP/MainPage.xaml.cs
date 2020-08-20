648b3869e3d6b964eee66b5ab4721e099b9a08d7
Anderson Lima <anderson.limaconsultor@inbursa.com>
2020-08-20 13:37:16 -0300

    Validacao do CEP

--- App01_ConsultaCEP/App01_ConsultaCEP/App01_ConsultaCEP/MainPage.xaml.cs
+++ App01_ConsultaCEP/App01_ConsultaCEP/App01_ConsultaCEP/MainPage.xaml.cs
@@ -22,9 +22,33 @@ namespace App01_ConsultaCEP
         {
             string cep = CEP.Text.Trim();
 
-            Endereco end = ViaCEPService.BuscarEnderecoViaCep(cep);
+            if (isValidCep(cep))
+            {
+                Endereco end = ViaCEPService.BuscarEnderecoViaCep(cep);
 
-            RESULTADO.Text = string.Format("Endereço: {0}, {1}, {2}" , end.Logradouro, end.Localidade, end.UF);
+                RESULTADO.Text = string.Format("Endereço: {0}, {1}, {2}", end.Logradouro, end.Localidade, end.UF);
+            }            
+        }
+
+        private bool isValidCep(string cep)
+        {
+            var isValid = true;
+
+            if (cep.Length != 8)
+            {
+                DisplayAlert("ERRO", "CEP Inválido! O Cep deve conter 8 caracteres.", "OK");
+
+                isValid = false;
+            }           
+
+            if(!int.TryParse(cep, out _))
+            {
+                DisplayAlert("ERRO", "CEP Inválido! O Cep deve conter apenas numeros.", "OK");
+
+                isValid = false;
+            }
+            
+            return isValid;
         }
     }
 }
