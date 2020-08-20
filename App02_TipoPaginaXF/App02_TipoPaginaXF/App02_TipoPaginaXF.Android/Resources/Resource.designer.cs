14aafbf3c147a6ffd525adf8bfc873e2a70b2023
Anderson Lima <anderson.limaconsultor@inbursa.com>
2020-08-20 19:00:35 -0300

    Carousel Page - Pagina 2 e 3 - ContentPage

--- App02_TipoPaginaXF/App02_TipoPaginaXF/App02_TipoPaginaXF.Android/Resources/Resource.designer.cs
+++ App02_TipoPaginaXF/App02_TipoPaginaXF/App02_TipoPaginaXF.Android/Resources/Resource.designer.cs
@@ -5788,13 +5788,19 @@ namespace App02_TipoPaginaXF.Droid
 			public const int notify_panel_notification_icon_bg = 2131165299;
 			
 			// aapt resource value: 0x7F070074
-			public const int tooltip_frame_dark = 2131165300;
+			public const int Page = 2131165300;
 			
 			// aapt resource value: 0x7F070075
-			public const int tooltip_frame_light = 2131165301;
+			public const int play = 2131165301;
 			
 			// aapt resource value: 0x7F070076
-			public const int xamarin = 2131165302;
+			public const int tooltip_frame_dark = 2131165302;
+			
+			// aapt resource value: 0x7F070077
+			public const int tooltip_frame_light = 2131165303;
+			
+			// aapt resource value: 0x7F070078
+			public const int xamarin = 2131165304;
 			
 			static Drawable()
 			{
