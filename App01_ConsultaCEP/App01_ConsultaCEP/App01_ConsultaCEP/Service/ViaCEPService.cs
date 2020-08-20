dd65e85aa1e8877e3a5f7c2d79482f93b6133888
Anderson Lima <anderson.limaconsultor@inbursa.com>
2020-08-20 10:32:28 -0300

    Classe Via Cep - Buscando da Internet o Endereco

--- App01_ConsultaCEP/App01_ConsultaCEP/App01_ConsultaCEP/Service/ViaCEPService.cs
+++ App01_ConsultaCEP/App01_ConsultaCEP/App01_ConsultaCEP/Service/ViaCEPService.cs
@@ -1,11 +1,26 @@
-﻿using System;
+﻿using App01_ConsultaCEP.Service.Model;
+using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Text;
+using Newtonsoft.Json;
 
 namespace App01_ConsultaCEP.Service
 {
     public class ViaCEPService
     {
         private static string EnderecoURL = "https://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoViaCep(string cep)
+        {
+            string novoEnderecoUrl = string.Format(EnderecoURL, cep);
+
+            WebClient wc = new WebClient();
+            var conteudo = wc.DownloadString(novoEnderecoUrl);
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(conteudo);
+
+            return end;
+        }
     }
 }
