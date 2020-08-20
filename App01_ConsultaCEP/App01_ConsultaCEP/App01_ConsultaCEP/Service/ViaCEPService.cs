a8c76dfcbe639838d704befa03c35d112c4f5583
Anderson Lima <anderson.limaconsultor@inbursa.com>
2020-08-20 09:50:13 -0300

    Criando Classe Endereco e Via Cep Servico

---
+++ App01_ConsultaCEP/App01_ConsultaCEP/App01_ConsultaCEP/Service/ViaCEPService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App01_ConsultaCEP.Service
+{
+    public class ViaCEPService
+    {
+        private static string EnderecoURL = "https://viacep.com.br/ws/{0}/json/";
+    }
+}
