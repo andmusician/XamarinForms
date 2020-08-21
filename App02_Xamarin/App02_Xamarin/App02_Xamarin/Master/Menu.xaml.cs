d6c82d35c04366c1d9b5deb10b2d4a9273acec34
Anderson Lima <anderson.limaconsultor@inbursa.com>
2020-08-21 20:27:24 -0300

    Criação do Projeto

---
+++ App02_Xamarin/App02_Xamarin/App02_Xamarin/Master/Menu.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App02_Xamarin.Master
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Menu : MasterDetailPage
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void GoPaginaPerfil1(object sender, EventArgs args)
+        {
+            Detail = new NavigationPage(new Pages.Perfil1());
+            IsPresented = false;
+        }
+
+        private void GoPaginaXamarin(object sender, EventArgs args)
+        {
+            Detail = new NavigationPage(new Pages.Xamarin());
+            IsPresented = false;
+        }
+
+    }
+}
