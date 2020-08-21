951f3bc9c6cbb76b067ef9949601f5066e180206
Anderson Lima <anderson.limaconsultor@inbursa.com>
2020-08-21 11:19:08 -0300

    Navigation Page - Modal

--- App02_TipoPaginaXF/App02_TipoPaginaXF/App02_TipoPaginaXF/TipoPagina/Navigation/Page1.xaml.cs
+++ App02_TipoPaginaXF/App02_TipoPaginaXF/App02_TipoPaginaXF/TipoPagina/Navigation/Page1.xaml.cs
@@ -21,5 +21,10 @@ namespace App02_TipoPaginaXF.TipoPagina.Navigation
         {
             Navigation.PushAsync(new Page2());
         }
+
+        private void ChamarModal(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new Modal());
+        }
     }
 }
