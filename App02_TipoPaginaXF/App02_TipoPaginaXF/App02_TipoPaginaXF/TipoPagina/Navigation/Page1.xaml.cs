6c7e56d5631b7a63d07c596bb7e287cc6921aa74
Anderson Lima <anderson.limaconsultor@inbursa.com>
2020-08-21 09:25:56 -0300

    Navigation Page - Criando Pagina 1 e 2

--- App02_TipoPaginaXF/App02_TipoPaginaXF/App02_TipoPaginaXF/TipoPagina/Navigation/Page1.xaml.cs
+++ App02_TipoPaginaXF/App02_TipoPaginaXF/App02_TipoPaginaXF/TipoPagina/Navigation/Page1.xaml.cs
@@ -7,19 +7,14 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace App02_TipoPaginaXF.TipoPagina.Carousel
+namespace App02_TipoPaginaXF.TipoPagina.Navigation
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class TipoPagina3 : ContentPage
+    public partial class Page1 : ContentPage
     {
-        public TipoPagina3()
+        public Page1()
         {
             InitializeComponent();
-        }
-
-        private void MudarPagina(object sender, EventArgs e)
-        {
-            Application.Current.MainPage = new Navigation.Page1();
         }
     }
 }
