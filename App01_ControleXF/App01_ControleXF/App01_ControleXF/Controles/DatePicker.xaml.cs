53592cb596ca9758567cb72ac12cf74158560140
Anderson Lima <anderson.limaconsultor@inbursa.com>
2020-08-27 18:16:24 -0300

    Controle - DatePicker

---
+++ App01_ControleXF/App01_ControleXF/App01_ControleXF/Controles/DatePicker.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App01_ControleXF.Controles
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DatePicker : ContentPage
+    {
+        public DatePicker()
+        {
+            InitializeComponent();
+        }
+
+        private void DatePicker_DateSelected(object sender, DateChangedEventArgs e)
+        {
+            lblResultado.Text = e.OldDate.ToString("dd/MM/yyyy") + " - " + e.NewDate.ToString("dd/MM/yyyy");
+        }
+    }
+}
